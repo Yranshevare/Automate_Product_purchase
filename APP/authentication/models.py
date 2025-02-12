3d8aec216b5eec4b0e6218aa39ba4364f80b4951
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-02-12 15:57:14 +0530

    created user model

--- APP/authentication/models.py
+++ APP/authentication/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import json
+
 
 # Create your models here.
+class UserModel(models.Model):
+    _id = models.AutoField(primary_key=True)
+    USER_TYPES = (('normal', 'Normal User'), ('admin', 'Administrator'))
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True,max_length=100)
+    password = models.CharField(max_length=50)
+    profession = models.CharField(max_length=100)
+    isEmailVerified = models.BooleanField(default=False)
+
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        OTHER = 'O', 'Other'
+
+    gender = models.CharField(
+        max_length=2, 
+        choices=Gender.choices, 
+        default=None
+    )
+
+    mobile = models.CharField(max_length=10)
+    process = models.TextField(default='[]')  # Store an empty JSON array by default
+    def set_process(self, value):
+        """Serialize the Python object to a JSON string."""
+        self.process = json.dumps(value)
+
+    def get_process(self):
+        """Deserialize the JSON string back to a Python object."""
+        return json.loads(self.process)
--- APP/authentication/views.py
+++ APP/authentication/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import make_password, check_password
+import json
 
 # Create your views here.
-
+@csrf_exempt
 def registerUser(request):
-    return JsonResponse({'message':"hello form auth app"})
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            data['isEmailVerified'] = False
+            password = data.get('password', None)
+            if password:
+                data['password'] = make_password(password)
+            else:
+                return JsonResponse({'error': 'Password not provided'}, status=400)
+
+            print(check_password(password, data['password']))
+            return JsonResponse({'data': data})
+        except Exception as e:
+            return JsonResponse({'error': 'error while registering user'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
