4b4b6494ed46f90ff7086008a42704e53ebc515f
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-04-02 23:17:25 +0530

    added RFQ model

--- Django_backend/APP/RFQ/models.py
+++ Django_backend/APP/RFQ/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class RFQModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=100)
+
+    sheet = models.TextField()
+    name = models.CharField(max_length=100)
+
+
+    class type(models.TextChoices):
+        SELECTED = 'Selected', 'Selected',
+        NOT_SELECTED = 'Not Selected', 'Not Selected'
+
+    type = models.CharField(max_length=20,choices=type.choices, default=type.NOT_SELECTED)
+
+    class status(models.TextChoices):
+        COMPLETED = 'Completed', 'Completed',
+        PENDING = 'Pending', 'Pending',
+        REJECTED = 'Rejected', 'Rejected'
+
+    status = models.CharField(max_length=20,choices=status.choices, default=status.PENDING)
+
+    process = models.ForeignKey('process.processModel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email
