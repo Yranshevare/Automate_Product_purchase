84d40bbe0439230978d4a1392f3bf89bdbd60f5c
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-04-13 18:35:15 +0530

    added backend url

--- React_Frontend/src/constant.js
+++ React_Frontend/src/constant.js
@@ -1,3 +1,4 @@
-export const server = 'http://127.0.0.1:8000/'
+export const server = 'https://automate-product-purchase-3.onrender.com/'
+// export const server = 'http://127.0.0.1:8000/'
 export const storeEmail = "store@gmail.com"
 export const accEmail = "acc@gmail.com"
