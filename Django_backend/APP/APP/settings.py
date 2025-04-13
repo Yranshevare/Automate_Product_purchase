a17dd9a5d5923820269b377191682ee6f7f672f9
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-04-13 17:35:08 +0530

    update the settings.py for deployement

--- Django_backend/APP/APP/settings.py
+++ Django_backend/APP/APP/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
+import os 
+import environ
+env = environ.Env()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--5vzsn@3e@=lkb2^lh!8wrpz3gd0pr))si@&%1e0&m4%-i)h4)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,24 +114,22 @@ WSGI_APPLICATION = 'APP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import os 
-import environ
-env = environ.Env()
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':env('DB_NAME'),
-        'USER':env('DB_USER'),
-        'PASSWORD':env('DB_PASSWORD'),
-        'HOST':env('DB_HOST'),
-        'PORT':env('DB_PORT')
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME':env('DB_NAME'),
+    #     'USER':env('DB_USER'),
+    #     'PASSWORD':env('DB_PASSWORD'),
+    #     'HOST':env('DB_HOST'),
+    #     'PORT':env('DB_PORT')
+    #     # 'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.config(
+        default=env('DB_URL'),
+        conn_max_age=600,
+    )
 }
 
 
@@ -201,3 +208,6 @@ FRONTEND = 'http://localhost:5173'
 
 
 STORE_EMAIL = 'yranshevare2005@gmail.com'
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
